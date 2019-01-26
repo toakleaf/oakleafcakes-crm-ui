8b871017a6fabe2d37a7947a2c7cefd17fa01c2a
tyleroakleaf <4921516+toakleaf@users.noreply.github.com>
2019-01-26 00:27:14 -0500

    started two new components

---
+++ src/components/Customers/Meta/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="column is-one-quarter"></div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Customers/Orderform/Orderform.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="column is-three-quarters"></div>
+</template>
+
+<script>
+export default {
+  name: "Orderform"
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
